eeae7d2d71b760be4df43a6b5b95bb8e03286180
Nicol <beltrannicole384@gmail.com>
2023-09-26 15:13:36 -0500

    :constrution: Add migrations municipalities and service_categories

--- database/migrations/2023_09_26_192816_create_municipalities_table.php
+++ database/migrations/2023_09_26_192816_create_municipalities_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('municipalities', function (Blueprint $table) {
             $table->id();
+            $table->string('name',30);
             $table->timestamps();
         });
     }
--- database/migrations/2023_09_26_193014_create_service__categories_table.php
+++ database/migrations/2023_09_26_193014_create_service__categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('service__categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name',45);
             $table->timestamps();
         });
     }
